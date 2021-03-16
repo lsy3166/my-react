32ebd7d8bbd887c65763c0749db8bd60eebdbb22
LSY3166\user <lsy3166@gmail.com>
2021-03-16 15:52:07 +0900

    react router dom 추가

--- src/App.js
+++ src/App.js
@@ -2,24 +2,38 @@ import Condition from "./components/Condition";
 import Count from "./components/Count";
 import Movie from "./components/Movie";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const title = "React World!";
   const movies = [];
   return (
-    <div className="App">
-      {/* {Component} */}
-      <Count name={title + 1} cl ick="Click First"></Count>
-      <Count name={title + 2}></Count>
-      <Count name={title + 3} click="Click Third"></Count>
-      <hr></hr>
-      {/* {조건 랜더링} */}
-      <Condition></Condition>
-      <hr></hr>
-      {/* {반복문 및 props} */}
-      {/* {부모창에 method props로 넘기기} */}
-      <Movie movies={movies}></Movie>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/movies">
+            {/* {반복문 및 props} */}
+            {/* {부모창에 method props로 넘기기} */}
+            <Movie movies={movies}></Movie>
+          </Route>
+          <Route path="/users">
+            <h1>Users</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+            {/* {Component} */}
+            <Count name={title + 1} cl ick="Click First"></Count>
+            <Count name={title + 2}></Count>
+            <Count name={title + 3} click="Click Third"></Count>
+            <hr></hr>
+            {/* {조건 랜더링} */}
+            <Condition></Condition>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
